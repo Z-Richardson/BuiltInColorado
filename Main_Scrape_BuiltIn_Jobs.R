f5e5975db46fb6faad042af85575fccd573cc149
Zachary Richardson <richardson.zachary.s@gmail.com>
2019-11-14 09:20:08 -0700

    Update Main_Scrape_BuiltIn_Jobs.R
    Changed to include more annotations and to also be more efficient in terms of iterating over the pages. Iteration is now dependent on the number of links currently available and no longer requires the user to input the max page number.

--- Main_Scrape_BuiltIn_Jobs.R
+++ Main_Scrape_BuiltIn_Jobs.R
@@ -1,23 +1,53 @@
-#Loading the rvest package
+# -----------------------------------------------------------------------------
+# Script Name: Main_Scrape_BuiltIn_Jobs
+#
+# Purpose of Script: Scrape job description data to use in analysis from 
+#                    https://www.builtincolorado.com for data science positions
+#
+# Author: Zachary Richardson, Ph.D.
+#
+# Date Created: 2019-11-14
+#
+# Copyright (c) Zachary Richardson, 2019
+# Email: zachinthelab@gmail.com
+# Blog: thelab.ghost.io
+# --------------------------------------
+#
+# Notes: Basic scrape of key information from Built In from the Colorado job
+#        board. This will focus on the 'Data Analysis' set of jobs but could
+#        expanded to include all of the jobs that are listed.
+#
+# -----------------------------------------------------------------------------
+# Load Packages 
 library(rvest)
 library(XML)
 library(dplyr)
 library(stringr)
 library(stringi)
 # ----------------------------------------------------------------------------------------
-# all.job.html <- 'https://www.builtincolorado.com/jobs?f[0]=job-category_data-analytics&hash-changes=2&page=0'
-all.job.html <- "https://www.builtincolorado.com/jobs?f[0]=job-category_data-analytics&f[1]=job-category_data-analytics-analytics&f[2]=job-category_data-analytics-data-science&hash-changes=7"
-
-page_result_start <- 0 # starting page 
-page_result_end <- 2 # last page results
-page_results <- seq(from = page_result_start, to = page_result_end, by = 1)
- 
+# ----------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------
+# Start with links to the primary job board and the data analytics job board specifically
+alljobs.url <- "https://www.builtincolorado.com/jobs"
+datajobs.url <- "https://www.builtincolorado.com/jobs?f[0]=job-category_data-analytics"
+# ----------------------------------------------------------------------------------------
+# First use Selector Gadget to get the URLs all the pages. (Note: Built In uses a zero
+# page number format so if there are 6 pages then the max page number will be 5 so it
+# seems more efficient to pull the links than to pull the values.)
+page.links <- read_html(datajobs.url) %>%
+  html_nodes('.js-pager__items') %>%
+  html_nodes('a') %>%
+  html_attr('href') %>%
+  unique()
+# One other note, I choose to mostly use CSS at first for the selector items since it's
+# shorter and easier to identify exactly wher ewe want to look at things.
+# --------------------------------------
+# --------------------------------------
+# Now itterate over the links to pull in the key info for each job description:
 full_df <- data.frame()
-for(i in seq_along(page_results)) {
-  first_page_url <- "https://www.builtincolorado.com/jobs?f[0]=job-category_data-analytics&hash-changes=2&page="
-  url <- paste0(first_page_url, page_results[i])
+for(i in page.links) {
+  url <- paste0(alljobs.url, i)
   page <- read_html(url)
-  # Sys.sleep(2)
 
   # get job titles
   job.title <- page %>%
